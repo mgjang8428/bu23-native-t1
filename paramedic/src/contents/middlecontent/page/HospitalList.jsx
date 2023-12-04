8cbfb10825e1aaab91b8e650ca3aeda5470263a9
HaLoomin <148430859+mgjang8428@users.noreply.github.com>
2023-12-05 00:27:12 +0900

    feat: 대원 어플 병원 리스트 기초 구현

--- paramedic/src/contents/middlecontent/page/HospitalList.jsx
+++ paramedic/src/contents/middlecontent/page/HospitalList.jsx
@@ -2,32 +2,40 @@ import { View, Text, Button } from "react-native";
 import React from "react";
 import { MiddlePageStateStore } from "@/stores/store";
 import styled from "styled-components/native";
+import Hospital from "../components/Hospital";
 
 export default function Page1() {
     return (
-        <PageView>
-            <Text style={{ fontSize: 30 }}>PageList</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-            <Text style={{ fontSize: 40 }}>1</Text>
-        </PageView>
+        <View>
+            <PageHeader>
+                <Text>Top</Text>
+            </PageHeader>
+            <PageScroll>
+                <Hospital />
+                <Hospital />
+                <Hospital />
+                <Hospital />
+                <Hospital />
+                <Hospital />
+                <Hospital />
+                <Hospital />
+                <Hospital />
+                <Hospital />
+                <Hospital />
+            </PageScroll>
+        </View>
     );
 }
 
-const PageView = styled.ScrollView`
+const ListMainView = styled.View`
     width: 100%;
     height: 100%;
+`;
+const PageHeader = styled.View`
+    width: 100%;
+    height: 10%;
+`;
+const PageScroll = styled.ScrollView`
+    width: 100%;
+    height: 90%;
 `;
